8534c17d5ac343f60575e3fd75a7498eda8c27ba
Jon Chin <myEmail>
2019-05-07 15:21:12 -0700

    Solve 6.1 Interconvert Strings and Integers

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,51 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+INT_TO_CHAR_OFFSET = 48
+
 
 def int_to_string(x):
-    # TODO - you fill in here.
-    return ''
+    digits = []
+    sign = ''
+
+    if x < 0:
+        sign = '-'
+        x *= -1
+    elif x == 0:
+        return '0'
+
+    while x > 0:
+        right_digit = x % 10
+        right_str = chr(right_digit + INT_TO_CHAR_OFFSET)
+        digits.append(right_str)
+        x //= 10
+
+    digits.append(sign)
+    digits.reverse()
+    final_str = ''.join(digits)
+
+    return final_str
 
 
 def string_to_int(s):
-    # TODO - you fill in here.
-    return 0
+    digits = []
+    sum = 0
+    multiplier = 1
+    sign = 1
+
+    if s[0] == '-':
+        sign = -1
+        s = s[1:]
+
+    for c in reversed(s):
+        digit = ord(c) - INT_TO_CHAR_OFFSET
+        digits.append(digit)
+
+    for digit in digits:
+        sum += digit * multiplier
+        multiplier *= 10
+
+    return sum * sign
 
 
 def wrapper(x, s):
