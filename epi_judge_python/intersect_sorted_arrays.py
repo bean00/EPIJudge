cb04b09f838bc49eee1e05c9c710c0cb37c5c1cf
Jon Chin <myEmail>
2019-05-31 13:24:32 -0700

    Solve 13.1 Compute the Intersection of Two Sorted Arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    i, j, arr = 0, 0, []
+
+    while i < len(A) and j < len(B):
+        if arr and A[i] == arr[-1]:
+            i += 1
+        elif A[i] < B[j]:
+            i += 1
+        elif A[i] == B[j]:
+            arr.append(A[i])
+            i, j = i + 1, j + 1
+        else:  # A[i] > B[j]
+            j += 1
+
+    return arr
 
 
 if __name__ == '__main__':
