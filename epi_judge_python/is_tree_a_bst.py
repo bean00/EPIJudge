5514c575afce0c28df672ece768ec3846fa04fe3
Jon Chin <myEmail>
2019-05-31 15:01:39 -0700

    Solve 14.1 Test if a Binary Tree Satisfies the BST Property

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    # TODO - you fill in here.
-    return True
+    return use_interval(tree, low_range, high_range)
+
+
+def use_interval(node, min, max):
+    if node is None:
+        return True
+
+    if not min <= node.data <= max:
+        return False
+
+    is_left_valid = use_interval(node.left, min, node.data)
+    is_right_valid = use_interval(node.right, node.data, max)
+
+    return is_left_valid and is_right_valid
 
 
 if __name__ == '__main__':
