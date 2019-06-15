2cba0947df109615b2dc4a3af9f2f06d3116be99
Jon Chin <myEmail>
2019-06-14 17:04:31 -0700

    Solve 6.4 Replace and Remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -5,14 +5,42 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size, s):
-    # TODO - you fill in here.
-    return 0
+    return do_in_place(size, s)
+
+
+# Avg runtime 20 us; Median runtime 4 us
+def do_in_place(size, s):
+    j = len(s) - 1
+
+    for i in reversed(range(size)):
+        if s[i] == 'b':
+            continue
+        elif s[i] == 'a':
+            s[j - 1:j + 1] = 'd', 'd'
+            j -= 2
+        else:  # s[i] is not 'a' or 'b'
+            s[j] = s[i]
+            j -= 1
+
+    del s[:j + 1]
+
+    return s
+
+
+def convert_then_replace(size, s):
+    s = ''.join(s)
+    s = s[:size]
+    s = s.replace('b', '')
+    s = s.replace('a', 'dd')
+    s = list(s)
+
+    return s
 
 
 @enable_executor_hook
 def replace_and_remove_wrapper(executor, size, s):
     res_size = executor.run(functools.partial(replace_and_remove, size, s))
-    return s[:res_size]
+    return s[:len(res_size)]
 
 
 if __name__ == '__main__':
