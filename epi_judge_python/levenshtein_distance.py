5ad5ca38cbd30ae514727260d591169e1ef85ce0
Jon Chin <myEmail>
2019-06-17 12:15:55 -0700

    Solve 16.2 Compute the Levenshtein Distance

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,33 @@
 from test_framework import generic_test
 
 
+# Avg runtime 980 us; Median runtime 450 us
 def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+    len_a, len_b = len(A), len(B)
+
+    arr = [[0] * (len_b + 1) for _ in range(len_a + 1)]
+
+    for i in range(len_a + 1):
+        arr[i][0] = i
+    for j in range(len_b + 1):
+        arr[0][j] = j
+
+    for i in range(1, len_a + 1):
+        for j in range(1, len_b + 1):
+            subs_cost = 0 if A[i - 1] == B[j - 1] else 1
+            min_dist = min(
+                arr[i - 1][j - 1] + subs_cost,  # substitute
+                arr[i][j - 1] + 1,              # insert
+                arr[i - 1][j] + 1               # delete
+            )
+            arr[i][j] = min_dist
+
+    return arr[len_a][len_b]
+
+
+def print_2d_list(li):
+    for row in li:
+        print(row)
 
 
 if __name__ == '__main__':
