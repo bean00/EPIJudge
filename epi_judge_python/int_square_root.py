6364b787de642c7a7185eb3952e812390e546595
Jon Chin <myEmail>
2019-06-18 11:25:23 -0700

    Solve 11.4 Compute the Integer Square Root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -1,9 +1,36 @@
 from test_framework import generic_test
 
+import math
+
 
 def square_root(k):
-    # TODO - you fill in here.
-    return 0
+    # return use_sqrt_function(k)
+    return use_binary_search(k)
+
+
+# Avg runtime 2 us; Median runtime 3 us
+def use_binary_search(k):
+    left, right = 0, k
+
+    # Candidate interval [left, right] where everything before left has
+    # square <= k, everything after right has square > k
+    while left <= right:
+        mid = (left + right) // 2
+        mid_squared = mid * mid
+
+        if mid_squared <= k:
+            left = mid + 1
+        else:  # mid_squared > k
+            right = mid - 1
+
+    return left - 1
+
+
+# Avg runtime 1 us; Median runtime 1 us
+def use_sqrt_function(k):
+    float_square_root = math.sqrt(k)
+    int_square_root = math.floor(float_square_root)
+    return int_square_root
 
 
 if __name__ == '__main__':
