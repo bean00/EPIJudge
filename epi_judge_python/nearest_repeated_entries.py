edcdf34bbe13251cb9147100372de1cf3f2ff79a
Jon Chin <myEmail>
2019-06-19 17:02:31 -0700

    Solve 12.5 Find the Nearest Repeated Entries in an Array

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
+from collections import defaultdict
+
 
 def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
-    return 0
+    min_dist = float('inf')
+    indices = defaultdict(int)
+
+    for i, word in enumerate(paragraph):
+        if word in indices:
+            last_i = indices[word]
+            dist = i - last_i
+            min_dist = min(dist, min_dist)
+        indices[word] = i
+
+    final_min_dist = min_dist if min_dist != float('inf') else -1
+    return final_min_dist
 
 
 if __name__ == '__main__':
