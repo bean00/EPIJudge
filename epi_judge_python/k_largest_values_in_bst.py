244f4eb59d52156a5b52470301bfacceb03ba6b5
Jon Chin <myEmail>
2019-06-21 12:33:14 -0700

    Solve 14.2 Find the First Key Greater than a Given Value in a BST

--- epi_judge_python/k_largest_values_in_bst.py
+++ epi_judge_python/k_largest_values_in_bst.py
@@ -2,8 +2,19 @@ from test_framework import generic_test, test_utils
 
 
 def find_k_largest_in_bst(tree, k):
-    # TODO - you fill in here.
-    return None
+    largest_elements = []
+    add_elements(tree, k, largest_elements)
+
+    return largest_elements
+
+
+def add_elements(node, k, largest_elements):
+    # Perform reverse in-order traversal
+    if node and len(largest_elements) < k:
+        add_elements(node.right, k, largest_elements)
+        if len(largest_elements) < k:
+            largest_elements.append(node.data)
+            add_elements(node.left, k, largest_elements)
 
 
 if __name__ == '__main__':
--- epi_judge_python/search_first_greater_value_in_bst.py
+++ epi_judge_python/search_first_greater_value_in_bst.py
@@ -2,8 +2,35 @@ from test_framework import generic_test
 
 
 def find_first_greater_than_k(tree, k):
-    # TODO - you fill in here.
-    return None
+    # return use_recursion(tree, k)
+    return use_iteration(tree, k)
+
+
+def use_recursion(tree, k):
+    result = find_greater_than_k(tree, k, None)
+    return result
+
+
+def find_greater_than_k(node, k, greater_key):
+    if node is None:
+        return greater_key
+
+    if node.data > k:
+        greater_key = node
+        return find_greater_than_k(node.left, k, greater_key)
+    else:  # node.data <= k
+        return find_greater_than_k(node.right, k, greater_key)
+
+
+def use_iteration(tree, k):
+    subtree, first_so_far = tree, None
+    while subtree:
+        if subtree.data > k:
+            first_so_far = subtree
+            subtree = subtree.left
+        else:  # Root and all keys in left subtree are <= k, so skip them
+            subtree = subtree.right
+    return first_so_far
 
 
 def find_first_greater_than_k_wrapper(tree, k):
