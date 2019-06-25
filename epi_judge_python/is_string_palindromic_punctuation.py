73b4c7995ccf76f31169839e6d3c7fdf8970b923
Jon Chin <myEmail>
2019-06-25 16:08:01 -0700

    Solve 6.5 Test Palindromicity

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,7 +2,17 @@ from test_framework import generic_test
 
 
 def is_palindrome(s):
-    # TODO - you fill in here.
+    clean_str = [c.lower() for c in s if c.isalnum()]
+    clean_str = ''.join(clean_str)
+
+    last_idx = len(clean_str) - 1
+    mid_idx = last_idx // 2
+
+    for i in range(mid_idx + 1):
+        back_idx = last_idx - i
+        if clean_str[i] != clean_str[back_idx]:
+            return False
+
     return True
 
 
