cda4cc749a95ec79a2d6ef35472c4cec5eb1d59e
Jon Chin <myEmail>
2019-06-25 12:53:12 -0700

    Solve 5.17 The Sudoku Checker Problem

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
 
-# Check if a partially filled matrix has any conflicts.
+# Avg runtime 26 us; Median runtime 27 us
 def is_valid_sudoku(partial_assignment):
-    # TODO - you fill in here.
+    rows = [[] for _ in range(9)]
+    cols = [[] for _ in range(9)]
+    sub_arrays = [[] for _ in range(9)]
+
+    for i in range(9):
+        for j in range(9):
+            val = partial_assignment[i][j]
+            if val != 0:
+                rows[i].append(val)
+                cols[j].append(val)
+
+                # add to appropriate list in sub_arrays
+                row_idx, col_idx = i // 3, j // 3
+                k = col_idx + (3 * row_idx)  # calc 0-8 from row (0-2) and col (0-2)
+                sub_arrays[k].append(val)
+
+    for li in rows + cols + sub_arrays:
+        li_as_set = set(li)
+        if len(li) != len(li_as_set):
+            return False
+
     return True
 
 
