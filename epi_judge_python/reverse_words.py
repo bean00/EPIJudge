b9eac64f6d73cec1fd599497f16a627dd6349dda
Jon Chin <myEmail>
2019-06-26 11:21:03 -0700

    Solve 6.6 Reverse All the Words in a Sentence

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -6,8 +6,42 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assume s is a string encoded as bytearray.
 def reverse_words(s):
-    # TODO - you fill in here.
-    return
+    for val in s:
+        print(val, chr(val))
+    # loop through s, creating list of words
+    words = []
+    word = []
+    for val in s:
+        if val == 32:
+            # if a previous word has been formed
+            if len(word) > 0:
+                words.append(word)
+                word = []
+            else:
+                words.append([32])
+        else:
+            word.append(val)
+
+    if len(word) > 0:  # TODO: remove this check?
+        words.append(word)
+
+    # reverse words list
+    words.reverse()
+
+    # join words with space (32)
+    joined = []
+    for word in words:
+        for val in word:
+            joined.append(val)
+        joined.append(32)
+
+    del joined[-1]
+
+    joined_as_bytes = bytearray(joined)
+    print(joined_as_bytes)
+    for i in range(len(s)):
+        s[i] = joined_as_bytes[i]
+    return s
 
 
 @enable_executor_hook
