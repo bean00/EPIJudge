bbcc8299019c33ee476e63940147f67b7379a9d3
Jon Chin <myEmail>
2019-06-28 14:53:28 -0700

    Solve 9.2 Test if a Binary Tree is Symmetric

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree):
-    # TODO - you fill in here.
-    return True
+    if not tree:
+        return True
+
+    is_symmetric = subtrees_equal(tree.left, tree.right)
+    return is_symmetric
+
+
+def subtrees_equal(left, right):
+    if not left and not right:  # or, 'if not left and not right'
+        return True
+    if (not left) or (not right) or (left.data != right.data):
+        return False
+
+    is_outside_symmetric = subtrees_equal(left.left, right.right)
+    is_inside_symmetric = subtrees_equal(left.right, right.left)
+
+    return is_outside_symmetric and is_inside_symmetric
 
 
 if __name__ == '__main__':
