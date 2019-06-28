6f9f44f11713fd3968ab24eac2bd6d8b94a97d66
Jon Chin <myEmail>
2019-06-28 10:42:35 -0700

    Solve 8.2 Evaluate RPN Expressions

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
+from collections import deque
+
 
 def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+    expression = expression.split(',')
+    stack = deque()
+    operators = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: int(x / y)
+    }
+
+    for elem in expression:
+        if elem in operators:
+            right_val = stack.pop()
+            left_val = stack.pop()
+            result = operators[elem](left_val, right_val)
+            stack.append(result)
+        else:
+            stack.append(int(elem))
+
+    final_result = stack.pop()
+    return final_result
 
 
 if __name__ == '__main__':
