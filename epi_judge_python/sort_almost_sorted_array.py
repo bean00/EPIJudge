2aced650f57ffeaaf120058282c5cf7d662ef918
Jon Chin <myEmail>
2019-07-02 12:27:03 -0700

    Solve 10.3 Sort an Almost-Sorted Array

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
 
+import heapq
+
 
 def sort_approximately_sorted_array(sequence, k):
-    # TODO - you fill in here.
-    return []
+    min_heap, sorted = [], []
+
+    for _ in range(k):
+        heapq.heappush(min_heap, next(sequence))
+
+    for elem in sequence:
+        smallest = heapq.heappushpop(min_heap, elem)
+        sorted.append(smallest)
+
+    rest_of_heap_sorted = heapq.nsmallest(k, min_heap)
+    sorted += rest_of_heap_sorted
+
+    return sorted
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
