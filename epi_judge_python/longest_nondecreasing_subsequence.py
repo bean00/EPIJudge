bab905dfa79509fab0a2f350158b6e21b5b027b5
Jon Chin <myEmail>
2019-07-03 13:34:49 -0700

    Solve 16.12 Find the Longest Nondecreasing Subsequence

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -2,8 +2,38 @@ from test_framework import generic_test
 
 
 def longest_nondecreasing_subsequence_length(A):
-    # TODO - you fill in here.
-    return 0
+    # return author_solution(A)
+    return my_solution(A)
+
+
+# Avg runtime 21 us; Median runtime 8 us
+# Compared to author: Avg 13X faster; Med 7X faster
+def my_solution(A):
+    result = []
+
+    for elem in A:
+        if not result or elem >= result[-1]:
+            result.append(elem)
+        else:
+            i = len(result) - 1
+            while i >= 0 and elem < result[i]:
+                i -= 1
+            result[i + 1] = elem
+
+    length = len(result)
+    return length
+
+
+# Avg runtime 274 us; Median runtime 52 us
+def author_solution(A):
+    # max_length[i] holds the length of the longest nondecreasing subsequence
+    # of A[:i + 1]
+    max_length = [1] * len(A)
+    for i in range(1, len(A)):
+        max_length[i] = max(1 + max(
+            (max_length[j] for j in range(i) if A[i] >= A[j]), default=0),
+                            max_length[i])
+    return max(max_length)
 
 
 if __name__ == '__main__':
