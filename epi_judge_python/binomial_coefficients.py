84a1d3a3371a2649d32234768c2d30c6575bab8d
Jon Chin <myEmail>
2019-07-08 16:51:00 -0700

    Solve 16.4 Compute the Binomial Coefficients

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -2,8 +2,35 @@ from test_framework import generic_test
 
 
 def compute_binomial_coefficient(n, k):
-    # TODO - you fill in here.
-    return 0
+    # return my_solution(n, k)
+    return author_solution(n, k)
+
+
+# Avg runtime 17 ms; Median runtime 442 us
+def my_solution(n, k):
+    result = [[1] * (k + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            result[i][j] = result[i][j - 1] + result[i - 1][j]
+
+    return result[n - k][k]
+
+
+# Avg runtime 1 ms; Median runtime 435 us
+def author_solution(n, k):
+    def compute_x_choose_y(x, y):
+        if y in (0, x):
+            return 1
+
+        if x_choose_y[x][y] == 0:
+            without_y = compute_x_choose_y(x - 1, y)
+            with_y = compute_x_choose_y(x - 1, y - 1)
+            x_choose_y[x][y] = without_y + with_y
+        return x_choose_y[x][y]
+
+    x_choose_y = [[0] * (k + 1) for _ in range(n + 1)]
+    return compute_x_choose_y(n, k)
 
 
 if __name__ == '__main__':
