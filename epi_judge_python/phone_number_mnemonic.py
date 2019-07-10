7ae00afd6c39d7ca8e5cd6e36730ee6e9a81ae1f
Jon Chin <myEmail>
2019-07-10 10:22:48 -0700

    Solve 6.7 Compute All Mnemonics for a Phone Number

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -1,9 +1,31 @@
 from test_framework import generic_test, test_utils
 
+import copy
+
+MAPPING = ('0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ')
+
 
 def phone_mnemonic(phone_number):
-    # TODO - you fill in here.
-    return []
+    return my_solution(phone_number)
+
+
+def my_solution(phone_number):
+    def build_sequences(digits, sequence):
+        if len(digits) == 0:
+            seq_str = ''.join(sequence)
+            sequences.append(seq_str)
+        else:
+            digit = int(digits[0])
+            for char in MAPPING[digit]:
+                seq_copy = copy.deepcopy(sequence)
+                seq_copy.append(char)
+                build_sequences(digits[1:], seq_copy)
+
+    digits = list(phone_number)
+    sequences = []
+    build_sequences(digits, [])
+
+    return sequences
 
 
 if __name__ == '__main__':
