22598fc1ffae8e0aa03a6079ccf11c96cb94013f
Jon Chin <myEmail>
2019-07-16 11:00:24 -0700

    Solve 7.10 Implement Even-Odd Merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
 
 
+# Avg runtime 26 us; Median runtime 6 us
 def even_odd_merge(L):
-    # TODO - you fill in here.
-    return None
+    if not L or not L.next:
+        return L
+
+    curr_ptr = L
+    next_ptr = odd_head = L.next
+    num_changes = 0
+
+    while next_ptr.next:
+        curr_ptr.next = next_ptr.next
+        curr_ptr = next_ptr
+        next_ptr = next_ptr.next
+        num_changes += 1
+
+    if num_changes % 2 == 0:
+        curr_ptr.next = odd_head
+    else:
+        next_ptr.next = odd_head
+        curr_ptr.next = None
+
+    return L
 
 
 if __name__ == '__main__':
