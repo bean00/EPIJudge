4f85614a506e0606b876939fd0c3a7dba085ab9b
Jon Chin <myEmail>
2019-07-16 16:30:12 -0700

    Solve 9.11 Implement an Inorder Traversal with O(1) Space

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -2,8 +2,31 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree):
-    # TODO - you fill in here.
-    return []
+    prev, result = None, []
+
+    while tree:
+        if prev is tree.parent:
+            # We came down to the tree from prev
+            if tree.left:
+                next = tree.left
+            else:
+                result.append(tree.data)
+                # Done with left, so go right if right is not empty
+                # Otherwise, go up
+                next = tree.right or tree.parent
+        elif tree.left is prev:
+            # We came up to tree from its left child
+            result.append(tree.data)
+            # Done with left, so go right if right is not empty
+            # Otherwise, go up
+            next = tree.right or tree.parent
+        else:  # Done with both children, so move up
+            next = tree.parent
+
+        prev, tree = tree, next
+        # tree = None
+
+    return result
 
 
 if __name__ == '__main__':
