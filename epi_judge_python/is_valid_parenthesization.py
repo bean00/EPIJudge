2dede73a9153c4391c4d6573f3de367d8a034b4f
Jon Chin <myEmail>
2019-07-16 11:39:54 -0700

    Solve 8.3 Test a String over "{,},(,),[,]" for Well-Formedness

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
 
+from collections import deque
 
+
+# Avg runtime 12 us; Median runtime 2 us
 def is_well_formed(s):
-    # TODO - you fill in here.
-    return True
+    d = deque()
+    char_map = {'{': '}', '(': ')', '[': ']'}
+
+    for char in s:
+        if char in char_map:
+            d.append(char)
+        elif not d or char_map[d.pop()] != char:
+            return False
+
+    is_d_empty = not d
+    return is_d_empty
 
 
 if __name__ == '__main__':
