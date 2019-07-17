599791a99b3a262e3fc7037912ce1abf0a475d12
Jon Chin <myEmail>
2019-07-17 15:11:04 -0700

    Solve 10.5 Compute the Median of Online Data

--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
 
+import heapq
+
 
 def online_median(sequence):
-    # TODO - you fill in here.
-    return []
+    min_heap = []
+    max_heap = []
+    result = []
+
+    for elem in sequence:
+        for_max = -heapq.heappushpop(min_heap, elem)
+        heapq.heappush(max_heap, for_max)
+
+        if len(max_heap) > len(min_heap):
+            for_min = -heapq.heappop(max_heap)
+            heapq.heappush(min_heap, for_min)
+
+        if len(min_heap) > len(max_heap):
+            curr_median = min_heap[0]
+        else:
+            curr_median = (min_heap[0] + -max_heap[0]) / 2
+
+        result.append(curr_median)
+
+    return result
 
 
 def online_median_wrapper(sequence):
