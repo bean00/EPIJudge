0735f2dad14c0ddc875025facdeb730577d4a65d
Jon Chin <myEmail>
2019-07-18 11:49:49 -0700

    Solve 11.5 Compute the Real Square Root

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,22 @@
 from test_framework import generic_test
 
+import math
+
 
 def square_root(x):
-    # TODO - you fill in here.
-    return 0.0
+    # Decides the search range according to x's value relative to 1.0
+    left, right = (x, 1.0) if x < 1.0 else (1.0, x)
+
+    # Keeps searching as long as left != right
+    while not math.isclose(left, right):
+        mid = 0.5 * (left + right)
+        mid_squared = mid * mid
+        if mid_squared > x:
+            right = mid
+        else:
+            left = mid
+
+    return left
 
 
 if __name__ == '__main__':
