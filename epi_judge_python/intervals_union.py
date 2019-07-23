a5dfd5652df543c4f67a8a450983365f15782da6
Jon Chin <myEmail>
2019-07-22 17:29:08 -0700

    Solve 13.8 Compute the Union of Intervals

--- epi_judge_python/intervals_union.py
+++ epi_judge_python/intervals_union.py
@@ -10,8 +10,56 @@ Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
 def union_of_intervals(intervals):
-    # TODO - you fill in here.
-    return []
+    # return my_solution(intervals)
+    return author_solution(intervals)
+
+
+def author_solution(intervals):
+    # Empty input
+    if not intervals:
+        return []
+
+    # Sort intervals according to left endpoints of intervals
+    intervals.sort(key=lambda i: (i.left.val, not i.left.is_closed))
+    result = [intervals[0]]
+
+    for i in intervals:
+        recent = result[-1]
+        if (i.left.val < recent.right.val or
+                (i.left.val == recent.right.val and (i.left.is_closed or recent.right.is_closed))):
+            if (i.right.val > recent.right.val or
+                    (i.right.val == recent.right.val and i.right.is_closed)):
+                result[-1] = Interval(recent.left, i.right)
+        else:
+            result.append(i)
+
+    return result
+
+
+def my_solution(intervals):
+    intervals.sort(key=lambda x: x.left.val)  # sort left-closed first?
+    # intervals.sort(key=lambda x: (x.left.val, 1 - x.left.is_closed))
+    # print_intervals(intervals)
+    result = []
+
+    for interval in intervals:
+        # get recently added 'recent' (result[-1]?)
+        recent = result[-1]  # handle index error when result is empty?
+        # if recent doesn't intersect with interval
+        intersect = recent.right.val > interval.left.val
+        endpoints_intersect = (recent.left.val == interval.left.val and recent.left.is_closed) or\
+                              (recent.right.val == interval.right.val and (recent.right.is_closed or
+                               interval.right.is_closed))
+            # add interval to result
+        # else # recent intersects with interval
+            # update recent to be the union with interval
+
+    return result
+
+
+def print_intervals(intervals):
+    for interval in intervals:
+        print((interval.left.val, interval.left.is_closed), (interval.right.val, interval.right.is_closed))
 
 
 @enable_executor_hook
