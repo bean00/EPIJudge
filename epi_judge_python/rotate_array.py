a57075ff66975c023e19fcc1aaf00121fb58bc08
Jon Chin <myEmail>
2019-07-30 12:29:25 -0700

    Solve 24.6 Rotate an Array

--- epi_judge_python/rotate_array.py
+++ epi_judge_python/rotate_array.py
@@ -4,9 +4,24 @@ from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 
+# Avg runtime 76 us; Median runtime 7 us
 def rotate_array(rotate_amount, A):
-    # TODO - you fill in here.
-    return
+    start, end = 0, len(A) - 1
+    rotate_amount %= len(A)
+    last_elem_i = rotate_amount - 1
+
+    reverse_in_place(A, start, end)
+
+    reverse_in_place(A, start, last_elem_i)
+    reverse_in_place(A, last_elem_i + 1, end)
+
+
+def reverse_in_place(A, start, end):
+    mid = (start + end) // 2
+
+    for i in range(start, mid + 1):
+        j = end - (i - start)
+        A[i], A[j] = A[j], A[i]
 
 
 @enable_executor_hook
