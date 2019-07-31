db13f8dd698206db418e227a29f29c9069ad20c6
Jon Chin <myEmail>
2019-07-30 17:06:23 -0700

    Solve 17.7 Compute the Maximum Water Trapped by a Pair of Vertical Lines

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -2,8 +2,26 @@ from test_framework import generic_test
 
 
 def get_max_trapped_water(heights):
-    # TODO - you fill in here.
-    return 0
+    return my_solution(heights)
+
+
+# Avg runtime 219 us; Median runtime 22 us
+def my_solution(heights):
+    max_area = 0
+    left, right = 0, len(heights) - 1
+
+    while left < right:
+        min_height = min(heights[left], heights[right])
+        width = right - left
+        area = min_height * width
+        max_area = max(area, max_area)
+
+        if heights[left] <= heights[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
 
 
 if __name__ == '__main__':
