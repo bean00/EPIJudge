9193790590e8060fce98a468605b7e5bcd62fac9
Jon Chin <myEmail>
2019-08-06 15:46:38 -0700

    Solve 7.11 Test Whether A Singly Linked List is Palindromic

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -2,7 +2,51 @@ from test_framework import generic_test
 
 
 def is_linked_list_a_palindrome(L):
-    # TODO - you fill in here.
+    return my_solution(L)
+
+
+def my_solution(L):
+    if not L or not L.next:
+        return True
+
+    mid = find_middle(L)
+
+    reversed_half = reverse_list(mid.next)
+
+    list_are_equal = are_lists_equal(L, reversed_half)
+    return list_are_equal
+
+
+def find_middle(L):
+    fast = slow = L
+
+    while fast.next and fast.next.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow
+
+
+def reverse_list(head):
+    prev = None
+    curr = head
+
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+    return prev
+
+
+def are_lists_equal(l1, l2):
+    # *Note: l1 includes l2, so check if l2 still exists
+    while l2:
+        if l1.data != l2.data:
+            return False
+        l1, l2 = l1.next, l2.next
+
     return True
 
 
